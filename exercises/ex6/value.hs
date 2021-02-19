644661608ae6816c38a2cf728c232b5e1710c6bb
jamalmohamad <mohammed.jamalhadi@gmail.com>
2021-02-19 15:05:04 +0100

    feat: solve value

--- exercises/ex6/value.hs
+++ exercises/ex6/value.hs
@@ -3,9 +3,8 @@ module Value where
     -- value 4 [(0,"c++"),(5,"python"),(4,"rust")] == "rust"
     -- value 4 [(0,"c++"),(5,"python"),(4,"go")]   == "go"
 
-    -- value n []        = ""
-    value n as@[(x, xs)]
-        | n == x        = xs
-        | otherwise     = value n (tail as)
+    value n []        = ""
+    value n ((x, str):xs) 
+        | n == x            = str
+        | otherwise         = value n xs
 
-    
